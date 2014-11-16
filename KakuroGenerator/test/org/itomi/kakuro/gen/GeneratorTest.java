b8c337a8547a3191cb35eb6284d1921a2f5e9177
Karol Kulesza <196012@student.pwr.wroc.pl>
2014-11-16 23:25:32 +0100

    Konfiguracja podprojektów i ich budowania i tworzenia paczek.

--- KakuroGenerator/test/org/itomi/kakuro/gen/GeneratorTest.java
+++ KakuroGenerator/test/org/itomi/kakuro/gen/GeneratorTest.java
@@ -1,5 +1,6 @@
 package org.itomi.kakuro.gen;
 
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,6 +9,7 @@ public class GeneratorTest {
 
 	private static final Logger LOG = LoggerFactory.getLogger(GeneratorTest.class);
 	
+	@Test
 	public void test_KakuroGeneratorBuildScriptTest() {
 		LOG.info("KakuroGenerator test project works!");
 	}
--- KakuroSolver/test/org/itomi/kakuro/solver/SolverTest.java
+++ KakuroSolver/test/org/itomi/kakuro/solver/SolverTest.java
@@ -1,11 +1,13 @@
 package org.itomi.kakuro.solver;
 
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SolverTest {
 
 	private static final Logger LOG = LoggerFactory.getLogger(SolverTest.class);
+	@Test
 	public void test_KakuroSolverBuildScriptTest() {
 		LOG.info("KakuroSolver project build script is working!");
 	}
